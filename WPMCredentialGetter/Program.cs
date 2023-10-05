86fde286650e371b7b0dd7f2c30948d31963dee0
Administrator <Administrator@WIN-FGKGGLDNDGT>
2023-10-05 13:35:32 -0700

    Push

--- WPMCredentialGetter/Program.cs
+++ WPMCredentialGetter/Program.cs
@@ -1,152 +1,243 @@
-﻿
-using Newtonsoft.Json;
-using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
 using System.Net.Http.Headers;
-using System.Net.Http.Json;
 using System.Net.Security;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using static Program;
 
-GetSecuredItems();
-
-//string bearerToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjVBNjlCMTMzQkQ2QTBFNDcwMTRCOTNGQzAxN0Y0MTExQjQwQ0VDNjkiLCJ4NXQiOiJXbW14TTcxcURrY0JTNVA4QVg5QkViUU03R2siLCJhcHBfaWQiOiJfX2lkYXB0aXZlX2N5YnJfdXNlcl9vaWRjIn0.eyJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXRoLmdhcmxldHQud29ya0BjeWJlcmFyay5jb20uNjU3NiIsImZhbWlseV9uYW1lIjoiV29yayIsInRlbmFudF9zdWJkb21haW4iOiIiLCJ1bmlxdWVfbmFtZSI6InNldGguZ2FybGV0dC53b3JrQGN5YmVyYXJrLmNvbS42NTc2IiwiaWRhcHRpdmVfdGVuYW50X2lkIjoiQUFZNDU1MSIsInBpY3R1cmUiOiJodHRwczovL2FheTQ1NTEuaWQuY3liZXJhcmsuY2xvdWQvVXNlck1nbXQvR2V0VXNlclBpY3R1cmU_aWQ9NzY3ODRhZDItMjBlYi00ZGFlLWIwNDktNTMwYTdlNDgwZjdkIiwidGVuYW50X2lkIjoiIiwidXNlcl9yb2xlcyI6WyJTYWxlc2ZvcmNlIENoYXR0ZXIgVXNlcnMiLCJQVldBIFVzZXJzIiwiU2FsZXNmb3JjZSBBZG1pbiBVc2VycyIsIkVQTSBVc2VycyIsIlN5c3RlbSBBZG1pbmlzdHJhdG9yIiwiRmxvd3MgQWRtaW4iLCJObyBNRkEiLCJTV1MgQWRtaW4iLCJBV1MgRnVsbCBDb250cm9sIiwiRXZlcnlib2R5IiwiTWVkaXVtIFJpc2sgVXNlcnMiXSwiaWF0IjoxNjk2NTIzNTkxLCJzdWIiOiI3Njc4NGFkMi0yMGViLTRkYWUtYjA0OS01MzBhN2U0ODBmN2QiLCJhdXRoX3RpbWUiOjE2OTY1MjM1OTEsImV4cCI6MTY5NjUyMzg5MSwidXNlcl91dWlkIjoiNzY3ODRhZDItMjBlYi00ZGFlLWIwNDktNTMwYTdlNDgwZjdkIiwic2NvcGUiOiJvcGVuaWQgYXBpIHByb2ZpbGUiLCJsYXN0X2xvZ2luIjoiMTY5NjUxMTU2MSIsImF1ZCI6Il9faWRhcHRpdmVfY3licl91c2VyX29pZGMiLCJhd3NfcmVnaW9uIjoiIiwic3ViZG9tYWluIjoiIiwiY3NyZl90b2tlbiI6IldmOVJJLTdGUVJha1NkTVBoSmNGMWVpUlhJdDNuLUZQczFpWWo5eXBuMEExIiwiaW50ZXJuYWxfc2Vzc2lvbl9pZCI6ImRkMGExNGUyLTE2MTktNDliYS1iMTVjLWQxOWQ5NjU2YmE0OCIsInBsYXRmb3JtX2RvbWFpbiI6IiIsImlzcyI6Imh0dHBzOi8vYWF5NDU1MS5pZC5jeWJlcmFyay5jbG91ZC9fX2lkYXB0aXZlX2N5YnJfdXNlcl9vaWRjLyIsImF0X2hhc2giOiJYR1piREMzcUtvMVdzSmR3QlZRVF9RIiwibmFtZSI6IlNldGggR2FybGV0dCBXb3JrIiwiZ2l2ZW5fbmFtZSI6IlNldGgiLCJFeHRlcm5hbFV1aWQiOiI3Njc4NGFkMi0yMGViLTRkYWUtYjA0OS01MzBhN2U0ODBmN2QifQ.bhww9aND8A3m3x0MyFKa57SBWMemAGOWksQRoBqNtk41IQPniI-rTmtPU8TL2U0rKzTzwmRTeP9BI77tZYpZHl_Yk2hnU_krmos2_fcCmsx2_tRy-Cw1FvfuJMAm7P9iyGYPFNFYdT5hxLMCrrZIuSpeO-LN0YOp68fsWlx2CoN-R1PZ9nVNMiC-H1hC8rp7ZnAkXzyUDkh5Cseva9eIGlowDO6pQ04DTLtRoE5eOh-lLmUGvkNR9e8deNJXzNT7C2zb7IFCn_fcvfS2cpnpbAS9BAsjQ7ObP_y75e0dTqxauw_Loj1Q3W5i6ZqGyLGwvkWZSdefYXIvUi83zH5OBA";
-
-
-static async Task<StartAuthDTO> StartAuthentication()
+class Program
 {
-    var startAuthDTO = new StartAuthDTO();
-    Console.WriteLine("Please enter your username:");
-    var userName = Console.ReadLine();
-
-
-    using var httpClient = new HttpClient();
-    try
+    private static string _TenantId { get; set; } = "aay4551";
+    private static string _CompoundedURL { get; set; } = $"https://{_TenantId}.id.cyberark.cloud";
+    private static string _PublicKey { get; set; } = "-----BEGIN PUBLIC KEY-----MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhxvr6R7Ery9qOwYVHy01OeHD4vW5g5my7EVlz9M14umetopZ3hM9S6x6CST4Tp7OhNytGDvYIOyO48XCGrWwSGYbCfHw4A4a8B3PrvY3TGjsQXgLWxQHePd7ITPsZi4I35dLJFoLYq4X5lcvD3CJPllaziqRylgSfcDjGYBUpJp1v3NHz9A7ZUg2NUaBNrxxOq0PORDhnkfewVbEeg4vdFnubLhyk98L+kkkzrvHWBO4ChugNbbIQY9FM7hzWxHAtiV5GrBqYSh8iWkWO5felGIRlZ16ygyZ2OOgZ5GLGpPpWlaJIzot1liKWfRIBhU41XKqQ2ZfkDwf73oOwIDAQAB-----END PUBLIC KEY-----";
+    static void Main()
     {
-        string apiUrl = "https://aay4551.id.cyberark.cloud/Security/StartAuthentication"; // Replace with your API endpoint URL
-                                                                                          // Serialize the payload to JSON
+        GetSecuredItems();
+
+        if (_SecuredItems.Count > 0)
+        {
+            int selectedIndex = 0;
+
+            while (selectedIndex > -1)
+            {
+                for (int i = 0; i < _SecuredItems.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {_SecuredItems[i].DisplayName}");
+                }
+
+                Console.Write("Enter the number of the item you want to select - Enter 0 to Exit: ");
+                if (int.TryParse(Console.ReadLine(), out selectedIndex))
+                {
+                    selectedIndex--; // Adjust for 0-based index
+
+                    if (selectedIndex < 0)
+                    {
+                        break;
+                    }
+
+                    var selectedItem = _SecuredItems[selectedIndex];
+                    Console.WriteLine($"Get credentials for {selectedItem.DisplayName}");
+                    var creds = GetSecuredItemCreds(selectedItem);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("No Results Found");
+        }
+    }
+    private static List<SecuredItem> _SecuredItems { get; set; }
+    static StartAuthDTO StartAuthentication()
+    {
+        var startAuthDTO = new StartAuthDTO();
+
+        Console.WriteLine("Please enter your username:");
+        var userName = Console.ReadLine();
+        var client = new RestClient($"{_CompoundedURL}/Security/StartAuthentication");
+        var request = new RestRequest(Method.POST);
+        request.AddHeader("Content-Type", "application/json");
+        request.Timeout = 30000; // 30 seconds timeout
+
         var payload = new
         {
-            TenantID = "aay4551",
+            TenantID = _TenantId,
             User = userName,
             Version = "1.0"
         };
-        string jsonPayload = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
-        var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
-        httpClient.Timeout = TimeSpan.FromSeconds(30); // Set a 30-second timeout
 
-        // Send the POST request with the payload
-        HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+        string jsonPayload = JsonConvert.SerializeObject(payload);
+        request.AddParameter("application/json", jsonPayload, ParameterType.RequestBody);
 
-        if (response.IsSuccessStatusCode)
+        try
         {
-            // Handle a successful response here
-            string responseContent = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"API Response: {responseContent}");
+            IRestResponse response = client.Execute(request);
 
-            // Deserialize the response content into your object (StartAuthDTO)
-            startAuthDTO = Newtonsoft.Json.JsonConvert.DeserializeObject<StartAuthDTO>(responseContent);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string responseContent = response.Content;
+                //Console.WriteLine($"API Response: {responseContent}");
+                JObject jsonObject = JObject.Parse(responseContent);
+                startAuthDTO.SessionId = jsonObject.SelectToken("Result.SessionId").ToString();
+                startAuthDTO.MechanismId = jsonObject.SelectToken("Result.Challenges[0].Mechanisms[0].MechanismId").ToString();
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
         }
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine($"Error: {response.StatusCode}");
+            Console.WriteLine($"HTTP Request Error: {ex.Message}");
+        }
+        return startAuthDTO;
+    }
+    public static string _BearerToken { get; set; }
+    static string GetBearerToken()
+    {
+        if (!string.IsNullOrEmpty(_BearerToken))
+        {
+            return _BearerToken;
+        }
+
+        var startAuthentication = StartAuthentication();
+
+        Console.WriteLine("Please enter your password:");
+        var password = Console.ReadLine();
+
+        var client = new RestClient($"{_CompoundedURL}/Security/AdvanceAuthentication");
+        var request = new RestRequest(Method.POST);
+        request.AddHeader("Content-Type", "application/json");
+
+        var payload = new
+        {
+            SessionId = startAuthentication.SessionId,
+            MechanismId = startAuthentication.MechanismId,
+            Action = "Answer",
+            Answer = password
+        };
+
+        string jsonPayload = JsonConvert.SerializeObject(payload);
+        request.AddParameter("application/json", jsonPayload, ParameterType.RequestBody);
+
+        try
+        {
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string responseContent = response.Content;
+                //Console.WriteLine($"API Response: {responseContent}");
+                JObject jsonObject = JObject.Parse(responseContent);
+                _BearerToken = jsonObject.SelectToken("Result.Token").ToString();
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"HTTP Request Error: {ex.Message}");
+        }
+        return _BearerToken;
+    }
+    static void GetSecuredItems()
+    {
+        var bearerToken = GetBearerToken();
+        var client = new RestClient($"{_CompoundedURL}/UPRest/GetSecuredItemsData");
+        var request = new RestRequest(Method.POST);
+        request.AddHeader("Content-Type", "application/json");
+        request.AddHeader("Authorization", $"Bearer {bearerToken}");
+
+        try
+        {
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string responseContent = response.Content;
+                SecuredItemsResponse securedItemsResponse = JsonConvert.DeserializeObject<SecuredItemsResponse>(responseContent);
+                _SecuredItems = new List<SecuredItem>();
+                _SecuredItems = securedItemsResponse.Result.SecuredItems;
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"HTTP Request Error: {ex.Message}");
         }
     }
-    catch (HttpRequestException ex)
+    static SelectedItemCreds GetSecuredItemCreds(SecuredItem securedItem)
     {
-        Console.WriteLine($"HTTP Request Error: {ex.Message}");
-    }
-    return startAuthDTO;
-}
-static async Task<string> GetBearerToken()
-{
-    var startAuthentication = await StartAuthentication();
-    var bearerToken = "";
+        var selectedItemCreds = new SelectedItemCreds();
+        var bearerToken = GetBearerToken();
+        var client = new RestClient($"{_CompoundedURL}/UPRest/GetCredsForSecuredItem?sItemKey=" + securedItem.ItemKey);
+        var request = new RestRequest(Method.POST);
+        request.AddHeader("Content-Type", "application/json");
+        request.AddHeader("Authorization", $"Bearer {bearerToken}");
 
-    Console.WriteLine("Please enter your password:");
-    var password = Console.ReadLine();
-
-    var payload = new
-    {
-        SessionId = startAuthentication.SessionId,
-        MechanismId = startAuthentication.MechanismId,
-        Action = "Answer",
-        Answer = password
-    };
-    string jsonPayload = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
-    var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
-    using var httpClient = new HttpClient();
-    try
-    {
-        string apiUrl = "https://aay4551.id.cyberark.cloud/Security/AdvanceAuthentication"; // Replace with your API endpoint URL
-
-        // Send the POST request with the payload
-        HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
-
-        if (response.IsSuccessStatusCode)
+        var payload = new
         {
-            // Handle a successful response here
-            string responseContent = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"API Response: {responseContent}");
+            publicKey = _PublicKey
+        };
 
-            // Deserialize the response content into your object (StartAuthDTO)
-            bearerToken = Newtonsoft.Json.JsonConvert.DeserializeObject<BearerTokenResult>(responseContent).BearerToken;
+        string jsonPayload = JsonConvert.SerializeObject(payload);
+        request.AddParameter("application/json", jsonPayload, ParameterType.RequestBody);
 
-        }
-        else
+        try
         {
-            Console.WriteLine($"Error: {response.StatusCode}");
+            IRestResponse response = client.Execute(request);
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string responseContent = response.Content;
+                Console.WriteLine($"API Response: {responseContent}");
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
         }
-    }
-    catch (HttpRequestException ex)
-    {
-        Console.WriteLine($"HTTP Request Error: {ex.Message}");
-    }
-    return bearerToken;
-}
-
-
-
-static async Task GetSecuredItems()
-{
-    var bearerToken = await GetBearerToken();
-    using var httpClient = new HttpClient();
-    try
-    {
-        string apiUrl = "https://aay4551.id.cyberark.cloud/UPRest/GetSecuredItemsData"; // Replace with your API endpoint URL
-                                                                                        // Serialize the payload to JSON
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
-
-        // Send the POST request with the payload
-        HttpResponseMessage response = await httpClient.PostAsync(apiUrl, null);
-
-        if (response.IsSuccessStatusCode)
+        catch (Exception ex)
         {
-            // Handle a successful response here
-            string responseContent = await response.Content.ReadAsStringAsync();
-            Console.WriteLine($"API Response: {responseContent}");
+            Console.WriteLine($"HTTP Request Error: {ex.Message}");
         }
-        else
-        {
-            Console.WriteLine($"Error: {response.StatusCode}");
-        }
+        return selectedItemCreds;
     }
-    catch (HttpRequestException ex)
+    public class StartAuthDTO
     {
-        Console.WriteLine($"HTTP Request Error: {ex.Message}");
+        [JsonProperty("Result.SessionId")]
+        public string SessionId { get; set; }
+        [JsonProperty("Result.Challenges[0].Mechanisms[0].MechanismId")]
+        public string MechanismId { get; set; }
     }
-}
-
-public class StartAuthDTO
-{
-    [JsonProperty("Result.SessionId")]
-    public string SessionId { get; set; }
-    [JsonProperty("Result.Challenges[0].Mechanisms[0].MechanismId")]
-    public string MechanismId { get; set; }
-}
-
-public class BearerTokenResult
-{
-    [JsonProperty("Result.Token")]
-    public string BearerToken { get; set; }
+    public class SecuredItemsResponse
+    {
+        public Result Result { get; set; }
+    }
+    public class Result
+    {
+        public List<SecuredItem> SecuredItems { get; set; }
+    }
+    public class SecuredItem
+    {
+        public string DisplayName { get; set; }
+        public string Name { get; set; }
+        public string ItemKey { get; set; }
+        public string SecuredItemType { get; set; }
+    }
+    public class SelectedItemCreds
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Note { get; set; }
+    }
 }
